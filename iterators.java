1d06780fee2395a51776470fb942eaf40d812d54
Ulduars <luis.gomezdelavega@outlook.com>
2024-11-15 18:07:24 -0500

    iterators.java

--- iterators.java
+++ iterators.java
@@ -3,34 +3,37 @@ import java.io.*;
 public class iterators{
     public static void main(String[] args){
 
-        LinkedList<String> names = new LinkedList<String>();
-        names.push("Luis");
-        names.push("Abhi");
-        names.push("Neil");
-        names.push("Yuno");
-        //FiLo <Yuno, Neil, Abhi, Luis>
+        LinkedList<String> names = new LinkedList<String>();        //FiLo <Yuno, Neil, Abhi, Luis>
+            names.push("Luis");
+            names.push("Abhi");
+            names.push("Neil");
+            names.push("Yuno");
+        
         Iterator<String> it = names.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next()); //Returns the value you skipped 'Yuno'
-        }
+        
+            while(it.hasNext()){
+                System.out.println(it.next()); //Returns the value you skipped 'Yuno'
+                }
             //Collection methods 
             names.add(2,"String"); // The 2 is the index we put it in
-        // For each 
-        System.out.println("\nFor each");
-        for(String s: names){
-            System.out.println(s);
-        }
+
+                // For each 
+            System.out.println("\nFor each");
+
+                for(String s: names){
+                System.out.println(s);
+                }
         //Alternative way
         //List Iterators
         ListIterator<String> its = names.listIterator();
-        its.next();
-        its.next();
-        its.add("Susan");
+            its.next();
+            its.next();
+            its.add("Susan");
 
         System.out.println("\nAlternative way");
-        for(String s: names){
-            System.out.println(s);
-        }
+            for(String s: names){
+                System.out.println(s);
+            }
 
     }
 }
